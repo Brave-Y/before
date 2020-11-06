d9228b5d507cb536d89d138f5134eb1d3291919f
Brave-Y <1175151069@qq.com>
2020-11-06 18:45:36 +0800

    完成登录

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 //初始化
 const express = require('express');
+const utility = require('utility');
 const app = express();
 app.listen(3006, () => {
     console.log('服务器启动了')
 });
+//加载数据库
+const sql = require('./sql')
+//配置
+app.use(express.urlencoded({ extended: false }))
+//--------------------------------注册
+app.post('/api/register', (req, res) => {
+    // console.log(req.body)
+    req.body.password = utility.md5(req.body.password)
+    sql('insert into user set ?', req.body, (err, result) => {
+        if (err) throw err;
+        if (result.affectedRows > 0) {
+            res.send({ status: 200, message: '注册成功' })
+        } else {
+            res.send({ status: 201, message: '注册失败' })
+        }
+    })
+})
+
+//---------------------登录
+app.post('/api/login', (req, res) => {
+    //console.log(req.body)
+    let username = req.body.username;
+    let password = utility.md5(req.body.password);
+    sql('select * from user where username=? and password=?', [username, password], (err, result) => {
+        if (err) throw err;
+        if (result.length > 0) {
+            res.send({
+                status: 200,
+                message: '登陆成功'
+            })
+        } else {
+            res.send({
+                status: 201,
+                message: '登录失败'
+            })
+        }
+    })
+})
--- sql.js
+++ sql.js
@@ -1,10 +1,10 @@
 //数据库初始化
-module.exports = function () {
+module.exports = function (sql, val, cb) {
     const mysql = require('mysql');
     const conn = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'toot',
+        password: 'root',
         database: 'before'
     })
     conn.connect();
